f2d081579e2166db10a13a60166e7f77a0d9820f
Kartik Tomar <ktomar466@gmail.com>
2025-01-21 16:39:02 +0530

    registered user on postman

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -33,8 +33,13 @@ const registerUser= asyncHandler(async(req,res)=>{
 
    //handling images,  as we have now introduced middleware it gives us access to more functionality
     const avatarLocalPath=req.files?.avatar[0]?.path;
-    const coverImageLocalPath=req.files?.coverImage[0]?.path;
-   
+    // const coverImageLocalPath=req.files?.coverImage[0]?.path;
+    let  coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage)&&req.files.coverImage.length>0)
+    {
+      coverImageLocalPath=req.files.coverImage[0].path;
+    }
+     
      if(!avatarLocalPath){
         throw new ApiError(400,"avatar file is required")
      }
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,7 +16,8 @@ const uploadOnCloudinary=async (localFilePath) =>{
             resource_type:"auto"
         })
          
-        console.log("File is succesfully uploaded on cloudinary", response.url);
+        // console.log("File is succesfully uploaded on cloudinary", response.url);
+        fs.unlinkSync(localFilePath)
         return response
         
     } catch(error){
